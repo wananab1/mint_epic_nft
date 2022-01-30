c4d61ec2e9b0c7de8db4d82988200d9b3eb80911
Shota Watanabe <wtnbshut.mij@gmail.com>
2022-01-30 22:00:23 +0900

    finished 4-1

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 // We need some util functions for strings.
@@ -16,13 +17,20 @@ contract MyEpicNFT is ERC721URIStorage {
 
   // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
   // So, we make a baseSvg variable here that all our NFTs can use.
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+  string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+  string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
 
   // I create three arrays, each with their own theme of random words.
   // Pick some random funny words, names of anime characters, foods you like, whatever!
   string[] firstWords = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
   string[] secondWords = ["Sushi", "Tempura", "Sashimi", "Ramen", "Sukiyaki", "Yakitori", "Wasabi"];
   string[] thirdWords = ["Alaska", "California", "Delaware", "Florida", "Georgia", "Hawaii", "Utah"];
+
+  // Get fancy with it! Declare a bunch of colors.
+  string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
+
+  event NewEpicNFTMinted(address sender, uint256 tokenId);
 
   constructor() ERC721 ("swatanab NFT", "swatanab") {
     console.log("This is my NFT contract. Woah!");
@@ -49,6 +57,13 @@ contract MyEpicNFT is ERC721URIStorage {
     return thirdWords[rand];
   }
 
+  // Same old stuff, pick a random color.
+  function pickRandomColor(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+    rand = rand % colors.length;
+    return colors[rand];
+  }
+
   function random(string memory input) internal pure returns (uint256) {
       return uint256(keccak256(abi.encodePacked(input)));
   }
@@ -60,10 +75,11 @@ contract MyEpicNFT is ERC721URIStorage {
     string memory first = pickRandomFirstWord(newItemId);
     string memory second = pickRandomSecondWord(newItemId);
     string memory third = pickRandomThirdWord(newItemId);
-	string memory combinedWord = string(abi.encodePacked(first, second, third));
+    string memory combinedWord = string(abi.encodePacked(first, second, third));
 
     // I concatenate it all together, and then close the <text> and <svg> tags.
-    string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+    string memory randomColor = pickRandomColor(newItemId);
+    string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
 
 	// Get all the JSON metadata in place and base64 encode it.
     string memory json = Base64.encode(
@@ -98,5 +114,7 @@ contract MyEpicNFT is ERC721URIStorage {
 
     _tokenIds.increment();
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+    emit NewEpicNFTMinted(msg.sender, newItemId);
   }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,8 @@ const main = async () => {
 	await nftContract.deployed();
 	console.log("Contract deployed to:", nftContract.address);
 
+	/**
+
 	// Call the function.
 	let txn = await nftContract.makeAnEpicNFT()
 	// Wait for it to be mined.
@@ -15,6 +17,8 @@ const main = async () => {
 	// Wait for it to be mined.
 	await txn.wait()
 	console.log("Minted NFT #2")
+
+	 */
   };
 
   const runMain = async () => {
